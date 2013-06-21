8b398b9ef3678074acfcb6117658e4e065fe4e77
TonyQ <tonylovejava@gmail.com>
2013-06-21 12:52:08 +0800

    finetune

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -9,8 +9,8 @@
   <div class="row">
     <div class="span12 ">
 		<h1> 如何取得邀請碼加入遊戲 </h1>
-		<p>若您需要邀請碼，請至 <a target="_blank" href="http://www.ingress.com/">官方網站</a> 透過 Request An Invit 並輸入您的 email 帳號取得邀請碼，<a href="<?=site_url("/Invite")?>">更多有關如何取得邀請碼的說明</a>。</p>
-		<p> <a href="<?=site_url("/Invite")?>"><img src="<?=base_url("img/step1.png")?>" /></a></p>
+		<p>若您需要邀請碼，請至 <a target="_blank" href="http://www.ingress.com/">官方網站</a> 透過 Request An Invit 並輸入您的 email 帳號取得邀請碼，<a href="<?=site_url("/invite")?>">更多有關如何取得邀請碼的說明</a>。</p>
+		<p> <a href="<?=site_url("/invite")?>"><img src="<?=base_url("img/step1.png")?>" /></a></p>
     </div>
   </div>
 </div>
