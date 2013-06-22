5d6f09228ecab1a63f15492c6a455f0a7dc95904
TonyQ <tonylovejava@gmail.com>
2013-06-22 16:15:07 +0800

    finetune

--- application/views/link.php
+++ application/views/link.php
@@ -13,9 +13,11 @@
 
 		<h1>其他網站</h1>
 
-		<a href="https://plus.google.com/communities/105093873281779892074">Ingress Taiwan 綠軍討論區</a>
-		<a href="https://plus.google.com/communities/110019129465742753783">Ingress Taiwan Resistance 藍軍討論區</a>
+		<a href="https://plus.google.com/communities/105093873281779892074">Ingress Taiwan Google Plus 台灣綠軍討論區</a>
+		<a href="https://plus.google.com/communities/110019129465742753783">Ingress Taiwan Resistance Google Plus 台灣藍軍討論區</a>
 		<a href="http://decodeingress.me/">Decode Ingress </a>
+		<a href="https://plus.google.com/communities/114606795989653285746">Decode Ingress Google Plus</a>
+
 
     </div>
   </div>
