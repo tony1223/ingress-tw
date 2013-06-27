6b717f3d3ccc8d487f6502796017beb760585468
TonyQ <tonylovejava@gmail.com>
2013-06-27 08:48:59 +0800

    finetune

--- application/models/historymodel.php
+++ application/models/historymodel.php
@@ -27,20 +27,20 @@ class HistoryModel extends MY_Model {
 
 	function insert_db(){
 
-//		$olds = $this->find_all_old();
-//
-//		foreach($olds as $kml){
-//			$this->db->insert(
-//				$this->_table,
-//				Array(
-//					"Type" => $kml->Title,
-//					"Title" => $kml->Title,
-//					"CreateDAte" => $kml->Time,
-//					"KML" => "http://ingress.tw/resources/status/".$kml->Kml,
-//					"Description" => $kml->Content
-//				)
-//			);
-//		}
+		$olds = $this->find_all_old();
+
+		foreach($olds as $kml){
+			$this->db->insert(
+				$this->_table,
+				Array(
+					"Type" => $kml->Title,
+					"Title" => $kml->Title,
+					"CreateDAte" => $kml->Time,
+					"KML" => "http://ingress.tw/resources/status/".$kml->Kml,
+					"Description" => $kml->Content
+				)
+			);
+		}
 
 
 	}
@@ -58,34 +58,42 @@ class HistoryModel extends MY_Model {
 				)),
 */
 			new FormObject(Array(
-				"Time" => "2013/06/25 12:00",
-				"Kml" => "20130625_120001_auto.kmz",
+				"Time" => "2013/06/26 12:00",
+				"Kml" => "20130626_120001_auto.kmz",
 				"Title" => " 雙方已佔領 portal 一覽",
-				"Content" => "取得 Portal 總數: 9465，綠軍 5624 個 (59.41%) ， 藍軍 3841 個 (40.58%)。
-連線數：4168，綠軍 2363 條 (56.69%) ， 藍軍 1805 條 (43.3%)。
-Control Field 數：1979，綠軍 1099 個 (55.53%) ， 藍軍 880 個 (44.46%)。
-MU：綠軍 110806 ，藍軍 214634。
+				"Content" => "取得 Portal 總數: 9526，綠軍 5705 個 (59.88%) ， 藍軍 3821 個 (40.11%)。
+連線數：4286，綠軍 2444 條 (57.02%) ， 藍軍 1842 條 (42.97%)。
+Control Field 數：1956，綠軍 1098 個 (56.13%) ， 藍軍 858 個 (43.86%)。
+MU：綠軍 96321 ，藍軍 248220。。
 				"
 			)),
 			new FormObject(Array(
-				"Time" => "2013/06/26 00:00",
-				"Kml" => "20130626_000001_auto.kmz",
-				"Title" => " 雙方已佔領 portal 一覽",
-				"Content" => "取得 Portal 總數: 9564，綠軍 5692 個 (59.51%) ， 藍軍 3872 個 (40.48%)。
-連線數：4271，綠軍 2454 條 (57.45%) ， 藍軍 1817 條 (42.54%)。
-Control Field 數：1892，綠軍 1057 個 (55.86%) ， 藍軍 835 個 (44.13%)。
-MU：綠軍 96151 ，藍軍 223168。
-				。
+				"Time" => "2013/06/26 18:00",
+				"Kml" => "20130626_180001_auto.kmz",
+				"Title" => "取得 Portal 總數: 9533，綠軍 5700 個 (59.79%) ， 藍軍 3832 個 (40.2%)。
+連線數：4284，綠軍 2485 條 (58%) ， 藍軍 1799 條 (41.99%)。
+Control Field 數：1972，綠軍 1127 個 (57.15%) ， 藍軍 845 個 (42.84%)。
+MU：綠軍 137074 ，藍軍 414459。
 				"
 			)),
 			new FormObject(Array(
-				"Time" => "2013/06/26 06:00",
-				"Kml" => "20130625_060003_auto.kmz",
+				"Time" => "2013/06/27 00:00",
+				"Kml" => "20130627_000001_auto.kmz",
 				"Title" => " 雙方已佔領 portal 一覽",
-				"Content" => "取得 Portal 總數: 9577，綠軍 5704 個 (59.55%) ， 藍軍 3873 個 (40.44%)。
-連線數：4268，綠軍 2451 條 (57.42%) ， 藍軍 1817 條 (42.57%)。
-Control Field 數：1888，綠軍 1053 個 (55.77%) ， 藍軍 835 個 (44.22%)。
-MU：綠軍 96180 ，藍軍 223168。"
+				"Content" => "取得 Portal 總數: 9616，綠軍 5881 個 (61.16%) ， 藍軍 3734 個 (38.83%)。
+連線數：4379，綠軍 2603 條 (59.44%) ， 藍軍 1776 條 (40.55%)。
+Control Field 數：2017，綠軍 1194 個 (59.19%) ， 藍軍 823 個 (40.8%)。
+MU：綠軍 157595 ，藍軍 384460。
+				"
+			)),
+			new FormObject(Array(
+				"Time" => "2013/06/27 06:00",
+				"Kml" => "20130627_060001_auto.kmz",
+				"Title" => " 雙方已佔領 portal 一覽",
+				"Content" => "取得 Portal 總數: 9599，綠軍 5902 個 (61.48%) ， 藍軍 3697 個 (38.51%)。
+連線數：4277，綠軍 2589 條 (60.53%) ， 藍軍 1688 條 (39.46%)。
+Control Field 數：1960，綠軍 1191 個 (60.76%) ， 藍軍 769 個 (39.23%)。
+MU：綠軍 157449 ，藍軍 381437。"
 			))
 		);
 	}
