f1a911ee0a27a7a8918cab3c47e13414d9185147
TonyQ <tonylovejava@gmail.com>
2013-07-04 23:24:09 +0800

    finetune

--- application/views/_site_footer.php
+++ application/views/_site_footer.php
@@ -38,7 +38,7 @@
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'UA-41902036-1', 'ingress.tw');
+	  ga('create', 'UA-42196530-1', 'ingress.tw');
 	  ga('send', 'pageview');
 
 	</script>
