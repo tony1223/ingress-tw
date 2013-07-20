d4ad96fe3763fc6c7834c934c723009526fcdbfa
TonyQ <tonylovejava@gmail.com>
2013-07-20 09:59:10 +0800

    finetune

--- application/controllers/pstatus.php
+++ application/controllers/pstatus.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Status extends MY_Controller {
+class PStatus extends MY_Controller {
 
 	public function index()
 	{
